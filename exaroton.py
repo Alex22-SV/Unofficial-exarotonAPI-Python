a4adf396e92fe881fc4ae6cfa9444382498cf34d
Alex0622 <aleximora2206@gmail.com>
2022-01-10 11:00:33 -0600

    Add a description before posting the results

--- exaroton.py
+++ exaroton.py
@@ -21,40 +21,61 @@ exarotonToken = input("Please enter your exaroton API token: ")
 def exaroton(optionID):
     optionID = int(optionID)
     if optionID == 0:
+        print('Loading...')
+        print('Option: 0')
+        print('Description: Get account information')
         path = "/account/"
         HTTPSoption = "GET"
         value = {}
         return path, HTTPSoption, value
     if optionID == 1:
+        print('Loading...')
+        print('Option: 1')
+        print('Description: List servers')
         path ="/servers/"
         HTTPSoption = "GET"
         value = {}
         return path, HTTPSoption, value
     if optionID == 2:
+        print('Loading...')
+        print('Option: 2')
+        print('Description: Get a server')
         serverID = input("Please enter the server ID: ")
         path = f"/servers/{serverID}/"
         HTTPSoption = "GET"
         value = {}
         return path, HTTPSoption, value
     if optionID == 3:
+        print('Loading...')
+        print('Option: 3')
+        print('Description: Get a server log')
         serverID = input("Please enter the server ID: ")
         path = f"/servers/{serverID}/logs/"
         HTTPSoption = "GET"
         value = {}
         return path, HTTPSoption, value
     if optionID == 4:
+        print('Loading...')
+        print('Option: 4')
+        print('Description: Upload a server log to mclo.gs')
         serverID = input("Please enter the server ID: ")
         path = f"/servers/{serverID}/logs/share/"
         HTTPSoption = "GET"
         value = {}
         return path, HTTPSoption, value
     if optionID == 5:
+        print('Loading...')
+        print('Option: 5')
+        print('Description: Get server RAM')
         serverID = input("Please enter the server ID: ")
         path = f"/servers/{serverID}/options/ram/"
         HTTPSoption = "GET"
         value = {}
         return path, HTTPSoption, value
     if optionID == 6:
+        print('Loading...')
+        print('Option: 6')
+        print('Description: Change server RAM')
         serverID = input("Please enter the server ID: ")
         value = input("Please enter the new amount of RAM: ")
         path = f"/servers/{serverID}/options/ram/"
@@ -62,12 +83,18 @@ def exaroton(optionID):
         value = {"ram": value}
         return path, HTTPSoption, value
     if optionID == 7:
+        print('Loading...')
+        print('Option: 7')
+        print('Description: Get server MOTD')
         serverID = input("Please enter the server ID: ")
         path = f"/servers/{serverID}/options/motd/"
         HTTPSoption = "GET"
         value = {}
         return path, HTTPSoption, value
     if optionID == 8:
+        print('Loading...')
+        print('Option: 8')
+        print('Description: Change server MOTD')
         serverID = input("Please enter the server ID: ")
         value = input("Please enter the new server MOTD: ")
         path = f"/servers/{serverID}/options/motd/"
@@ -75,24 +102,36 @@ def exaroton(optionID):
         value = {"motd": value}
         return path, HTTPSoption, value
     if optionID == 9:
+        print('Loading...')
+        print('Option: 9')
+        print('Description: Start a server')
         serverID = input("Please enter the server ID: ")
         path = f"/servers/{serverID}/start/"
         HTTPSoption = "GET"
         value = {}
         return path, HTTPSoption, value
     if optionID == 10:
+        print('Loading...')
+        print('Option: 10')
+        print('Description: Stop a server')
         serverID = input("Please enter the server ID: ")
         path = f"/servers/{serverID}/stop/"
         HTTPSoption = "GET"
         value = {}
         return path, HTTPSoption, value
     if optionID == 11:
+        print('Loading...')
+        print('Option: 11')
+        print('Description: Restart a server')
         serverID = input("Please enter the server ID: ")
         path = f"/servers/{serverID}/restart/"
         HTTPSoption = "GET"
         value = {}
         return path, HTTPSoption, value
     if optionID == 12:
+        print('Loading...')
+        print('Option: 12')
+        print('Description: Execute a server command')
         serverID = input("Please enter the server ID: ")
         value = input("Please enter the command to execute: ")
         path = f"/servers/{serverID}/command/"
@@ -100,12 +139,18 @@ def exaroton(optionID):
         value = {"command": value}
         return path, HTTPSoption, value
     if optionID == 13:
+        print('Loading...')
+        print('Option: 13')
+        print('Description: Get available player lists')
         serverID = input("Please enter the server ID: ")
         path = f"/servers/{serverID}/playerlists/"
         HTTPSoption = "GET"
         value = {}
         return path, HTTPSoption, value
     if optionID == 14:
+        print('Loading...')
+        print('Option: 14')
+        print('Description: Get player list contents')
         serverID = input("Please enter the server ID: ")
         list = input("Please enter the list: ")
         path = f"/servers/{serverID}/playerlists/{list}"
